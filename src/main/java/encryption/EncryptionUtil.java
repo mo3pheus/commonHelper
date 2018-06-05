1d6742cb4cb1f6b34ff36757ca34013f94f198ab
Sanket <sanket.korgaonkar@gmail.com>
2018-06-04 21:06:54 -0500

    Feature: Moved EncryptionUtil from Coms lib

--- src/main/java/encryption/EncryptionUtil.java
+++ src/main/java/encryption/EncryptionUtil.java
@@ -1,74 +1,72 @@
-package encryption;
+package communications.encryption;
 
 import certificates.RsaSecureComsCertificate;
 import space.exploration.communications.protocol.security.SecureMessage;
 
 import javax.crypto.Cipher;
 import java.io.*;
-import java.security.*;
+import java.security.Signature;
 
 public class EncryptionUtil {
-    public static boolean verifyMessage(File certificate, SecureMessage.SecureMessagePacket message) throws
-            NoSuchProviderException, NoSuchAlgorithmException, IOException, ClassNotFoundException,
-            InvalidKeyException, SignatureException {
-        RsaSecureComsCertificate comsCertificate = extractCertificateObject(certificate);
-        byte[]                   givenSignature  = message.getSignature().toByteArray();
-        byte[]                   content         = message.getContent().toByteArray();
 
-        Signature sig = Signature.getInstance("SHA1withDSA", "SUN");
-        sig.initVerify(comsCertificate.getSignaturePubKey());
+    private static RsaSecureComsCertificate extractCertificate(File certFile) throws IOException,
+            ClassNotFoundException {
+        FileInputStream          fileInputStream   = new FileInputStream(certFile);
+        ObjectInputStream        objectInputStream = new ObjectInputStream(fileInputStream);
+        RsaSecureComsCertificate comsCertificate   = (RsaSecureComsCertificate) objectInputStream.readObject();
+        return comsCertificate;
+    }
 
-        BufferedInputStream bufferedInputStream = new BufferedInputStream(new ByteArrayInputStream(content));
-        byte[]              buffer              = new byte[1024];
-        int                 len;
-        while (bufferedInputStream.available() != 0) {
-            len = bufferedInputStream.read(buffer);
-            sig.update(buffer, 0, len);
+    public static boolean verifyMessage(File certificate, SecureMessage.SecureMessagePacket secureMessagePacket)
+            throws Exception {
+        RsaSecureComsCertificate comsCertificate   = extractCertificate(certificate);
+        byte[]                   signatureToVerify = secureMessagePacket.getSignature().toByteArray();
+        Signature                signature         = Signature.getInstance("SHA1withDSA", "SUN");
+        signature.initVerify(comsCertificate.getSignaturePubKey());
+
+        BufferedInputStream bufin = new BufferedInputStream(new ByteArrayInputStream(secureMessagePacket.getContent()
+                                                                                             .toByteArray()));
+
+        byte[] buffer = new byte[10485760];
+        int    len;
+        while (bufin.available() != 0) {
+            len = bufin.read(buffer);
+            signature.update(buffer, 0, len);
         }
-        bufferedInputStream.close();
+        bufin.close();
 
-        return sig.verify(givenSignature);
-    }
-
-    public static byte[] decryptMessage(File certificate, SecureMessage.SecureMessagePacket messagePacket) throws
-            Exception {
-        RsaSecureComsCertificate comsCertificate  = extractCertificateObject(certificate);
-        byte[]                   encryptedContent = messagePacket.getContent().toByteArray();
-
-        Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
-        cipher.init(Cipher.DECRYPT_MODE, comsCertificate.getMessagePrvKey());
-
-        return cipher.doFinal(encryptedContent);
-    }
-
-    public static byte[] encryptMessage(File certificate, byte[] rawContent) throws Exception {
-        RsaSecureComsCertificate comsCertificate = extractCertificateObject(certificate);
-        Cipher                   cipher          = Cipher.getInstance("RSA/ECB/PKCS1Padding");
-        cipher.init(Cipher.ENCRYPT_MODE, comsCertificate.getMessagePubKey());
-
-        return cipher.doFinal(rawContent);
+        return signature.verify(signatureToVerify);
     }
 
     public static byte[] signMessage(File certificate, byte[] encryptedContent) throws Exception {
-        RsaSecureComsCertificate comsCertificate = extractCertificateObject(certificate);
+        RsaSecureComsCertificate comsCertificate = extractCertificate(certificate);
         Signature                dsa             = Signature.getInstance("SHA1withDSA", "SUN");
         dsa.initSign(comsCertificate.getSignaturePrvKey());
 
         BufferedInputStream bufin  = new BufferedInputStream(new ByteArrayInputStream(encryptedContent));
-        byte[]              buffer = new byte[1024];
+        byte[]              buffer = new byte[10485760];
         int                 len;
         while ((len = bufin.read(buffer)) >= 0) {
             dsa.update(buffer, 0, len);
         }
         bufin.close();
-        return dsa.sign();
+
+        byte[] signature = dsa.sign();
+        return signature;
     }
 
-    private static RsaSecureComsCertificate extractCertificateObject(File certificateFile) throws IOException,
-            ClassNotFoundException {
-        FileInputStream          fileInputStream          = new FileInputStream(certificateFile);
-        ObjectInputStream        objectInputStream        = new ObjectInputStream(fileInputStream);
-        RsaSecureComsCertificate rsaSecureComsCertificate = (RsaSecureComsCertificate) objectInputStream.readObject();
-        return rsaSecureComsCertificate;
+    public byte[] encryptMessage(File certificate, byte[] rawContent) throws Exception {
+        RsaSecureComsCertificate comsCertificate = extractCertificate(certificate);
+        Cipher                   cipher          = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+        cipher.init(Cipher.ENCRYPT_MODE, comsCertificate.getMessagePubKey());
+        return cipher.doFinal(rawContent);
+    }
+
+    public byte[] decryptMessage(File certificate, byte[] encryptedContent) throws Exception {
+        RsaSecureComsCertificate comsCertificate = extractCertificate(certificate);
+
+        Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+        cipher.init(Cipher.DECRYPT_MODE, comsCertificate.getMessagePrvKey());
+        return cipher.doFinal(encryptedContent);
     }
 }
