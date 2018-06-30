1362d41e2921f0e7c82a5eb44dca252741989901
Dylan Dsouza <dsouzadyn@gmail.com>
2018-06-30 17:34:57 +0530

    Moved compression to encryptionutils and added tests

--- src/main/java/encryption/EncryptionUtil.java
+++ src/main/java/encryption/EncryptionUtil.java
@@ -5,6 +5,8 @@ import com.google.protobuf.ByteString;
 import domain.SecureResult;
 import exceptions.DataIntegrityException;
 import exceptions.SignatureVerificationFailureException;
+import org.apache.commons.compress.compressors.deflate.DeflateCompressorInputStream;
+import org.apache.commons.compress.compressors.deflate.DeflateCompressorOutputStream;
 import space.exploration.communications.protocol.security.SecureMessage;
 
 import javax.crypto.BadPaddingException;
@@ -20,6 +22,40 @@ import java.util.concurrent.*;
 
 public class EncryptionUtil {
     public static final int ENCRYPTION_BLOCK_SIZE = 2000;
+    private static final int BUFFER_SIZE = 10485760;
+
+    public synchronized static byte[] compress(byte[] data) throws IOException {
+        BufferedInputStream in = new BufferedInputStream(new ByteArrayInputStream(data));
+        ByteArrayOutputStream fOut = new ByteArrayOutputStream();
+        BufferedOutputStream out = new BufferedOutputStream(fOut);
+
+        DeflateCompressorOutputStream compressorOut = new DeflateCompressorOutputStream(out);
+
+        byte[] buffer = new byte[BUFFER_SIZE];
+        int n;
+        while(-1 != (n = in.read(buffer))) {
+            compressorOut.write(buffer, 0, n);
+        }
+        compressorOut.close();
+        in.close();
+        return fOut.toByteArray();
+    }
+
+    public synchronized static byte[] decompress(byte[] data) throws IOException {
+        BufferedInputStream in = new BufferedInputStream(new ByteArrayInputStream(data));
+        ByteArrayOutputStream fOut = new ByteArrayOutputStream();
+
+        DeflateCompressorInputStream compressorIn = new DeflateCompressorInputStream(in);
+
+        byte[] buffer = new byte[BUFFER_SIZE];
+        int n;
+        while(-1 != (n = compressorIn.read(buffer))) {
+            fOut.write(buffer, 0, n);
+        }
+        compressorIn.close();
+        in.close();
+        return fOut.toByteArray();
+    }
 
     public synchronized static RsaSecureComsCertificate extractCertificate(File certFile) throws IOException,
             ClassNotFoundException {
---
+++ src/test/java/encryption/ImageCompressionTest.java
@@ -0,0 +1,91 @@
+package encryption;
+
+import com.google.protobuf.ByteString;
+import junit.framework.TestCase;
+import org.junit.Test;
+import space.exploration.communications.protocol.communication.RoverStatusOuterClass;
+import space.exploration.communications.protocol.security.SecureMessage;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class ImageCompressionTest extends TestCase {
+    final String SEPARATOR      = "=============================================================================";
+    final String THIN_SEPARATOR = "-----------------------------------------------------------------------------";
+    final String VERTICAL_SEPERATOR = "|";
+    File clientCertificate   = new File("src/main/resources/encryptionKeys/client.ser");
+    File                              serverCertificate   = new File("src/main/resources/encryptionKeys/server.ser");
+    File                              imageFile           = new File("src/main/resources/data/img.png");
+    SecureMessage.SecureMessagePacket secureMessagePacket = null;
+    RoverStatusOuterClass.RoverStatus roverStatus         = null;
+
+
+    private String constructOutput(String fileType, String algorithm, boolean isCompressionEnabled, long time) {
+        String out = String.format(" %10s ", fileType);
+        out += VERTICAL_SEPERATOR;
+        out += String.format(" %16s ", algorithm);
+        out += VERTICAL_SEPERATOR;
+        if(!isCompressionEnabled) {
+            out += String.format(" %18s ", "No");
+        } else {
+            out += String.format(" %18s ", "Yes");
+        }
+        out += VERTICAL_SEPERATOR;
+        out += " " + time;
+        return out;
+    }
+
+    @Override
+    public void setUp() throws IOException {
+        RoverStatusOuterClass.RoverStatus.Builder rBuilder = RoverStatusOuterClass.RoverStatus.newBuilder();
+        rBuilder.setSolNumber(100);
+        byte[] content = Files.readAllBytes(Paths.get(imageFile.getPath()));
+        rBuilder.setModuleMessage(ByteString.copyFrom(content));
+        rBuilder.setNotes("This is a test with a secret image message.");
+        roverStatus = rBuilder.build();
+        System.out.println("     Sample | Algorithm Choice | compressionEnabled | timeInMilliseconds");
+    }
+
+    @Test
+    public void testImageCompressionEnabled() {
+        try {
+            long start = System.currentTimeMillis();
+            secureMessagePacket = EncryptionUtil.encryptData("Server", serverCertificate,
+                    EncryptionUtil.compress(roverStatus.toByteArray()), 1);
+            byte[] decryptedContent = EncryptionUtil.decompress(EncryptionUtil.decryptSecureMessage
+                    (clientCertificate, secureMessagePacket, 1));
+            long stop = System.currentTimeMillis();
+            String output = constructOutput("Image", "Parallel", true,
+                    stop - start);
+            System.out.println(output);
+            RoverStatusOuterClass.RoverStatus roverStatusNew = RoverStatusOuterClass.RoverStatus.parseFrom
+                    (decryptedContent);
+            assertTrue(roverStatusNew.equals(roverStatus));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testImageCompressionDisabled() {
+        try {
+            long start = System.currentTimeMillis();
+            secureMessagePacket = EncryptionUtil.encryptData("Server", serverCertificate,
+                    roverStatus.toByteArray(), 1);
+            byte[] decryptedContent = EncryptionUtil.decryptSecureMessage
+                    (clientCertificate, secureMessagePacket, 1);
+            long stop = System.currentTimeMillis();
+            String output = constructOutput("Image", "Parallel", false,
+                    stop - start);
+            System.out.println(output);
+            RoverStatusOuterClass.RoverStatus roverStatusNew = RoverStatusOuterClass.RoverStatus.parseFrom
+                    (decryptedContent);
+            assertTrue(roverStatusNew.equals(roverStatus));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
